9b7c3bad5b25e81cc5d09ed0ee50dd26d0adffa0
thaiannguyen <thaiandev05@gmail.com>
2026-09-10 21:22:20 +0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const ans = new Map<string, string[]>();
+
+        for (let i = 0; i < strs.length; i++) {
+            const sortedElement = strs[i].split("").sort().join("");
+
+            if (ans.has(sortedElement)) {
+                ans.get(sortedElement)!.push(strs[i]);
+            } else {
+                ans.set(sortedElement, [strs[i]]);
+            }
+        }
+
+        return [...ans.values()]
+    }
+}
