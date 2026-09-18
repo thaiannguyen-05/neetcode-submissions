c16e39ced6c44f85c7c2c49f88cb9defa90af6bf
thaiannguyen <thaiandev05@gmail.com>
2026-09-18 14:22:53 +0700

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.ts
@@ -0,0 +1,9 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s: string[]): void {
+        s.reverse();
+    }
+}
