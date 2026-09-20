f41b4faaefe3d0ca4ef42b20ad3fda8a60ccf155
thaiannguyen <thaiandev05@gmail.com>
2026-09-20 14:35:18 +0700

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    majorityElement(nums: number[]): number[] {
+        const ans: number[] = [];
+        const map = new Map<number, number>();
+        const guard = Math.trunc(nums.length/3);
+
+        for(let i = 0 ; i<nums.length ; i++) {
+            if(map.has(nums[i])) {
+                map.set(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.set(nums[i], 0);
+            }
+        }
+
+        for(const [k,v] of map) {
+            if(v >= guard) {
+                ans.push(k);
+            }
+        }
+
+        console.log(map)
+        console.log(guard)
+
+        return ans;
+    }
+}
