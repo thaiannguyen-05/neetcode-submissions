c0b2eb661aff11d4ae70350c7a33c2943cce3b8a
thaiannguyen <thaiandev05@gmail.com>
2026-09-21 07:23:00 +0700

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1: string, word2: string): string {
+        let merge: string = "";
+        let i = 0;
+        let j = 0;
+
+        while(i < word1.length && j < word2.length) {
+            merge += word1[i];
+            merge += word2[i];
+            console.log(word1[i])
+            console.log(word2[j])
+            ++i;
+            ++j;
+        }
+
+        while(i < word1.length) {
+            merge += word1[i];
+            ++i;
+        }
+
+        while(j < word2.length) {
+            merge += word2[j];
+            ++j;
+        }
+
+        return merge;
+
+    }
+}
