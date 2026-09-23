41cadfe987290abc390cda99a9994bb68072efac
thaiannguyen <thaiandev05@gmail.com>
2026-09-23 07:09:08 +0700

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums: number[]): number {
+        let k = 1;
+
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] !== nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
